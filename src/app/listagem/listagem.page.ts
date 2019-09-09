f0b21a104cd5534de51faeac98ba8ba9687c1064
caikeburgos <caikegburgos@hotmail.com>
2019-09-09 20:10:33 -0300

    Commit Inicial

---
+++ src/app/listagem/listagem.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listagem',
+  templateUrl: './listagem.page.html',
+  styleUrls: ['./listagem.page.scss'],
+})
+export class ListagemPage implements OnInit {
+
+	public itens: Array<{ titulo: string; descricao: string }> = [];
+
+  constructor() { 
+
+  	for (let i = 0; i < 5; i++) {
+      this.itens.push(
+      {
+        titulo: 'Elemento ' + (i+1),
+        descricao: 'This is item #' + (i+1)
+      }
+      );
+
+  }
+}
+
+  ngOnInit() {
+  }
+
+}
