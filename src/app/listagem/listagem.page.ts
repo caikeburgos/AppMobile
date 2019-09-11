1253b2270142af64663ef15747d7a34ead787263
caikeburgos <caikegburgos@hotmail.com>
2019-09-10 22:18:25 -0300

    Conexao com API

--- src/app/listagem/listagem.page.ts
+++ src/app/listagem/listagem.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-listagem',
@@ -8,18 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class ListagemPage implements OnInit {
 
 	public itens: Array<{ titulo: string; descricao: string }> = [];
+  public posts;
 
-  constructor() { 
+  constructor(private apiService : ApiService) { 
 
-  	for (let i = 0; i < 5; i++) {
-      this.itens.push(
-      {
-        titulo: 'Elemento ' + (i+1),
-        descricao: 'This is item #' + (i+1)
-      }
-      );
+    this.apiService.getPosts().subscribe((data)=>{
+      console.log(data);
+      this.posts = data;
+    });
 
-  }
+  	// for (let i = 0; i < 5; i++) {
+   //    this.itens.push(
+   //    {
+   //      titulo: 'Elemento ' + (i+1),
+   //      descricao: 'This is item #' + (i+1)
+   //    }
+   //    );
+
+  //}
 }
 
   ngOnInit() {
